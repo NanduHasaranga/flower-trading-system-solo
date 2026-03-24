515bdc6c3f11db2791d810bc44bacf719a1b0510
NanduHasaranga <nandu.hasaranga@gmail.com>
2026-03-24 10:00:28 +0530

    Initially completed OrderBookSide header

--- include/Engine/OrderBookSide.hpp
+++ include/Engine/OrderBookSide.hpp
@@ -1,5 +1,64 @@
 #ifndef ORDERBOOKSIDE_HPP
 #define ORDERBOOKSIDE_HPP
 
+#include <map>
+#include <deque>
+#include <stdexcept>
+#include "./Core/Order.hpp"
+
+struct BuyComparator {
+    bool operator()(double a, double b) const {
+        return a > b; // highest first
+    }
+};
+
+struct SellComparator {
+    bool operator()(double a, double b) const {
+        return a < b; //lowest first
+    }
+};
+
+template <typename Comparator>
+class OrderBookSide {
+    private:
+        std::map<double, std::deque<Order>, Comparator> orders;
+
+    public:
+        OrderBookSide() : orders(Comparator{}) {}
+
+        void insertOrder(const Order &order) {
+            orders[order.price].push_back(order);
+        }
+
+        const Order &getBestOrder() const {
+            if(orders.empty()) {
+                throw std::runtime_error("OrderBook is Empty.");
+            }
+            return orders.begin()->second.front();
+        }
+
+        void removeTopOrder() {   
+            if(orders.empty())
+                return;
+
+            auto &bestLevelQueue = orders.begin()->second;
+            bestLevelQueue.pop_front();
+
+            if(bestLevelQueue.empty()) {
+                orders.erase(orders.begin());
+            }
+        }
+
+        void updateTopOrderQuantity(int newQty){
+            if(orders.empty())
+                return;
+
+            orders.begin()->second.front().quantity = newQty;
+        }
+
+        bool isEmpty() const {
+            return orders.empty();
+        }
+};
 
 #endif
