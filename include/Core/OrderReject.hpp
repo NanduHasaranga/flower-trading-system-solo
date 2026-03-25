fcc8f04f9c44fa89261552ee08878bef1c7a1d01
3nethz <ggthineth@gmail.com>
2026-03-25 23:38:32 +0530

    create orderreject constructor

--- include/Core/OrderReject.hpp
+++ include/Core/OrderReject.hpp
@@ -2,6 +2,7 @@
 #define REJECTIONREPORT_HPP
 
 #include <string>
+#include <utility>
 
 struct OrderReject
 {
@@ -14,6 +15,22 @@ struct OrderReject
     std::string timestamp;
 
     OrderReject() = default;
+        OrderReject(std::string clientOrderId,
+                                std::string instrument,
+                                std::string side,
+                                std::string price,
+                                std::string quantity,
+                                std::string reason,
+                                std::string timestamp)
+                : clientOrderId(std::move(clientOrderId)),
+                    instrument(std::move(instrument)),
+                    side(std::move(side)),
+                    price(std::move(price)),
+                    quantity(std::move(quantity)),
+                    reason(std::move(reason)),
+                    timestamp(std::move(timestamp))
+        {
+        }
 };
 
 #endif
