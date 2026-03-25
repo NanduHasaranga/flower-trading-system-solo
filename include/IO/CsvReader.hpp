f01176b7d1f4babf5ae7e0d6666be97a26d60740
3nethz <ggthineth@gmail.com>
2026-03-25 08:33:05 +0530

    start implementing CsvReader class

---
+++ include/IO/CsvReader.hpp
@@ -0,0 +1,12 @@
+#ifndef CSVREADER_HPP
+#define CSVREADER_HPP
+
+#include <string>
+#include <vector>
+
+class CsvReader {
+    public:
+        std::vector<std::string> parseLine(const std::string &line) const;
+};
+
+#endif
