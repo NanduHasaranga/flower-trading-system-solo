789470de0186b8237eee34b1852c1560c000947a
3nethz <ggthineth@gmail.com>
2026-03-25 23:40:08 +0530

    remove unused methods

--- include/Core/Types.hpp
+++ include/Core/Types.hpp
@@ -1,10 +1,7 @@
 #ifndef TYPES_HPP
 #define TYPES_HPP
 
-#include <string>
 #include <array>
-#include <unordered_map>
-#include <stdexcept>
 
 enum class Side
 {
@@ -32,26 +29,10 @@ enum class Instrument
 constexpr std::array<const char *, 5> InstrumentStrings = {
     "Rose", "Lavender", "Lotus", "Tulip", "Orchid"};
 
-constexpr std::array<const char *, 2> SideStrings = {
-    "Buy", "Sell"};
-
-constexpr std::array<const char *, 4> OrderStatusStrings = {
-    "New", "Reject", "Fill", "PFill"};
-
 // Enum → string (fast O(1) lookup)
 inline constexpr const char *to_string(Instrument i)
 {
     return InstrumentStrings[static_cast<int>(i)];
-}
-
-inline constexpr const char *to_string(Side s)
-{
-    return SideStrings[static_cast<int>(s)];
-}
-
-inline constexpr const char *to_string(OrderStatus o)
-{
-    return OrderStatusStrings[static_cast<int>(o)];
 }
 
 #endif
