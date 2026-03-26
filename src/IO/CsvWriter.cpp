0e07b0a34b33abfb2478fbd9e252e514b7f0de39
3nethz <ggthineth@gmail.com>
2026-03-26 18:18:50 +0530

    remove unwanted functions in csvwriter

--- src/IO/CsvWriter.cpp
+++ src/IO/CsvWriter.cpp
@@ -11,7 +11,7 @@ void CsvWriter::writeExecutions(const std::string &path, const std::vector<Execu
     for (const auto &r : rejects)
     {
         file << r.orderId << ","
-             << r.clientOrderId << ","             
+             << r.clientOrderId << ","
              << r.instrument << ","
              << r.side << ","
              << r.status << ","
@@ -20,61 +20,3 @@ void CsvWriter::writeExecutions(const std::string &path, const std::vector<Execu
              << r.reason << "\n";
     }
 }
-
-// void CsvWriter::writeExecutions(const std::string &path, const std::vector<ExecutionReport> &reports)
-// {
-//     std::ofstream file(path);
-//     file << "Order ID,Client Order ID,Instrument,Side,Exec Status,Quantity,Price,Reason,Transaction Time\n";
-
-//     for (const auto &r : reports)
-//     {
-//         file << r.orderId << ","
-//              << r.clientOrderId << ","
-//              << to_string(r.instrument) << ","
-//              << static_cast<int>(r.side) << ","
-//              << static_cast<int>(r.status) << ","
-//              << r.quantity << ","
-//              << std::fixed << std::setprecision(2) << r.price << ","
-//              << ","
-//              << r.transactionTime << "\n";
-//     }
-// }
-
-// void CsvWriter::writeChronological(const std::string &path, const std::vector<Record> &records)
-// {
-//     std::ofstream file(path);
-//     file << "Order ID,Client Order ID,Instrument,Side,Exec Status,Quantity,Price,Reason,Transaction Time\n";
-
-//     for (const auto &record : records)
-//     {
-//         std::visit([&file](auto &&arg)
-//                    {
-//             using T = std::decay_t<decltype(arg)>;
-//             if constexpr (std::is_same_v<T, ExecutionReport>)
-//             {
-//                 const auto& r = arg;
-//                  file << r.orderId << ","
-//                      << r.clientOrderId << ","
-//                      << to_string(r.instrument) << ","
-//                      << static_cast<int>(r.side) << ","
-//                      << static_cast<int>(r.status) << ","
-//                      << r.quantity << ","
-//                      << std::fixed << std::setprecision(2) << r.price << ","
-//                      << ","
-//                      << r.transactionTime << "\n";
-//             }
-//             else if constexpr (std::is_same_v<T, OrderReject>)
-//             {
-//                 const auto& r = arg;
-//                  file << ","
-//                      << r.clientOrderId << ","
-//                      << r.instrument << ","
-//                      << r.side << ","
-//                      << static_cast<int>(OrderStatus::Reject) << ","
-//                      << r.quantity << ","
-//                      << r.price << ","
-//                      << r.reason << ","
-//                      << r.timestamp << "\n";
-//             } }, record);
-//     }
-// }
