776055807a6b9e2288d2585eae2fb0d8f7eadcc9
3nethz <ggthineth@gmail.com>
2026-03-26 18:43:34 +0530

    remove unwanted functions

--- include/Core/Types.hpp
+++ include/Core/Types.hpp
@@ -1,25 +1,16 @@
 #ifndef TYPES_HPP
 #define TYPES_HPP
 
-#include <string>
 #include <array>
-#include <unordered_map>
-#include <stdexcept>
 
-enum class Side {
+enum class Side
+{
     Buy = 0,
     Sell = 1
 };
 
-enum class OrderStatus {
-    New = 0,
-    Reject = 1,
-    Fill = 2,
-    PFill = 3
-
-};
-
-enum class Instrument {
+enum class Instrument
+{
     Rose,
     Lavender,
     Lotus,
@@ -27,30 +18,20 @@ enum class Instrument {
     Orchid
 };
 
-constexpr std::array<const char*, 5> InstrumentStrings = {
-    "Rose", "Lavender", "Lotus", "Tulip", "Orchid"
-};
+constexpr std::array<const char *, 5> InstrumentStrings = {
+    "Rose", "Lavender", "Lotus", "Tulip", "Orchid"};
 
-constexpr std::array<const char*, 2> SideStrings = {
-    "1", "2"
-};
+constexpr std::array<const char *, 2> SideStrings = {
+    "1", "2"};
 
-constexpr std::array<const char*, 4> OrderStatusStrings = {
-    "New", "Reject", "Fill", "PFill"
-};
-
-// Enum → string (fast O(1) lookup)
-inline constexpr const char* to_string(Instrument i) {
+inline constexpr const char *to_string(Instrument i)
+{
     return InstrumentStrings[static_cast<int>(i)];
 }
 
-inline constexpr const char* to_string(Side s) {
+inline constexpr const char *to_string(Side s)
+{
     return SideStrings[static_cast<int>(s)];
 }
 
-inline constexpr const char* to_string(OrderStatus o) {
-    return OrderStatusStrings[static_cast<int>(o)];
-}
-
-
-#endif 
+#endif
