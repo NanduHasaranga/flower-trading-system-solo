4bfd6c36399a31b871e9c6fab53ecbefc61c5d53
3nethz <ggthineth@gmail.com>
2026-03-26 21:34:38 +0530

    improve orderbook processing use emplace_back, reduce redundant stiring conversions

--- include/Core/ExecutionReport.hpp
+++ include/Core/ExecutionReport.hpp
@@ -2,6 +2,7 @@
 #define EXECUTIONREPORT_HPP
 
 #include <string>
+#include <utility>
 
 struct ExecutionReport
 {
@@ -26,6 +27,6 @@ struct ExecutionReport
         std::string quantity,
         std::string status,
         std::string reason,
-        std::string transactionTime) : clientOrderId(clientOrderId), orderId(orderId), instrument(instrument), side(side), price(price), quantity(quantity), status(status), reason(reason), transactionTime(transactionTime) {}
+        std::string transactionTime) : clientOrderId(std::move(clientOrderId)), orderId(std::move(orderId)), instrument(std::move(instrument)), side(std::move(side)), price(std::move(price)), quantity(std::move(quantity)), status(std::move(status)), reason(std::move(reason)), transactionTime(std::move(transactionTime)) {}
 };
 #endif
